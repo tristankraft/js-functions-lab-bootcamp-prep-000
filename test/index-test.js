c897cc86b390e3763456e0593fb6b8f55d375c89
Tristan Kraft <tristankraft@gmail.com>
2018-08-23 20:31:42 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,6 +9,10 @@ describe('functions', () => {
     })
   })
 
+
+function happyHolidaysTo (name) {
+  return "Happy holidays, ${name}"
+}
   describe('happyHolidaysTo(name)', () => {
     it('returns "Happy holidays, ${name}!"', () => {
       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
